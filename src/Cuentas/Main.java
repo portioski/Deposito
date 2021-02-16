2d79461a346abe333de4031d4ec123e52c721d69
Oscar <Oscar@TOSHblan>
2021-02-16 11:28:21 +0100

    Version 1 Programa para añadir y retirar cantidad de dinero

---
+++ src/Cuentas/Main.java
@@ -0,0 +1,37 @@
+package Cuentas;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner escaner= new Scanner(System.in);
+        
+        System.out.println("Introduce la cantidad:");
+        float cantidad = escaner.nextFloat();
+
+        
+        operativa_cuenta(cantidad);
+    }
+
+    public static void operativa_cuenta(float cantidad) {
+        CCuenta cuenta1;
+        double saldoActual;
+
+        cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es" + saldoActual);
+
+        try {
+            cuenta1.retirar(cantidad);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(cantidad);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
